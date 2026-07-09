42e3981323996c74855198f659bab3776850447a
Анастасия <avgust.05@list.ru>
2026-07-10 00:43:10 +0300

    .

---
+++ 3e.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[0][0] = a[0][0];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 && j == 0) continue;
+
+            int best = -1;
+            if (i > 0) best = max(best, dp[i-1][j]);
+            if (j > 0) best = max(best, dp[i][j-1]);
+            if (i > 0 && j > 0) best = max(best, dp[i-1][j-1]);
+
+            dp[i][j] = best + a[i][j];
+        }
+    }
+
+    cout << dp[n-1][m-1] << endl;
+    return 0;
+}
